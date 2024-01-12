f8bf1f9eb66e36d252a1763af4ff524dba0c2c33
Tim Engle <timothy.englew@gmail.com>
2024-01-11 17:00:58 -0800

    broke test

--- index.test.js
+++ index.test.js
@@ -1,22 +1,22 @@
-const sayHello = require('./index');
+const sayHello = require("./index");
 
-describe('sayHello', () => {
-    it("should return 'Hello, World!' if no parameters were passed", () => {
-        const expected = 'Hello, World!';
-        const actual = sayHello();
+describe("sayHello", () => {
+  it("should return 'Hello, World!' if no parameters were passed", () => {
+    const expected = "Hello, World!";
+    const actual = sayHello();
 
-        expect(actual).toBe(expected);
-    });
+    expect(actual).toBe(expected);
+  });
 
-    it.each([
-        [['Bob'], 'Hello, Bob!'],
-        [['Bob', 'Steve'], 'Hello, Bob, Steve!'],
-    ])(
-        "should return strings separated by ', ' if an array of string are passed in",
-        (names, expected) => {
-            const actual = sayHello(names);
+  it.each([
+    [["Bob"], "Hello, Bob!"],
+    [["Bob", "Steve"], "Hello, Bob, Steve"],
+  ])(
+    "should return strings separated by ', ' if an array of string are passed in",
+    (names, expected) => {
+      const actual = sayHello(names);
 
-            expect(actual).toBe(expected);
-        }
-    );
+      expect(actual).toBe(expected);
+    }
+  );
 });
